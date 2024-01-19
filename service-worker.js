7d9ef2d6de5a4c6c885b40b1bb8275a2ae6061a1
DHoper <gxgemini777@gmail.com>
2024-01-19 13:53:06 +0800

    fix-up

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,17 @@
 // service-worker.js
 
-self.addEventListener('install', (event) => {
-    event.waitUntil(
-      caches.open('TEST').then((cache) => {
-        return cache.addAll([
-          '//assets',
-          '/index.html'
-        ]);
-      })
-    );
-  });
-  
-  self.addEventListener('fetch', (event) => {
-    event.respondWith(
-      caches.match(event.request).then((response) => {
-        return response || fetch(event.request);
-      })
-    );
-  });
-  
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open("TEST").then((cache) => {
+      return cache.addAll(["/assest", "/index.html", "/inde.js"]);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
