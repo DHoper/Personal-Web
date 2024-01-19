e819122ea9ddd6f1ae7d30cd58905657aaaf6187
DHoper <gxgemini777@gmail.com>
2024-01-19 14:11:31 +0800

    fix-up

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 console.log(navigator, 99999);
 if ("serviceWorker" in navigator) {
   navigator.serviceWorker
-    .register("/service-worker.js")
+    .register("./service-worker.js")
     .then((registration) => {
       console.log("Service Worker registered with scope:", registration.scope);
 
@@ -9,7 +9,7 @@ if ("serviceWorker" in navigator) {
       setInterval(displayNotification, 10000);
     })
     .catch((error) => {
-      console.error("Service Worker registration failed:", error);
+      console.error("服務註冊錯誤:", error);
     });
 }
 
