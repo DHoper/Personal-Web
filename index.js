3b3a35986cf7352b6ae6e9d321ce355e5efc8f9d
DHoper <gxgemini777@gmail.com>
2024-01-19 14:32:47 +0800

    fix-up

--- index.js
+++ index.js
@@ -4,9 +4,20 @@ if ("serviceWorker" in navigator) {
     .register("./service-worker.js")
     .then((registration) => {
       console.log("Service Worker registered with scope:", registration.scope);
+      alert(777);
 
-      // 在 Service Worker 註冊成功後，開始定時推送通知
-      setInterval(displayNotification, 10000);
+      // 請求推送權限
+      if (Notification.permission !== "granted") {
+        Notification.requestPermission().then((permission) => {
+          if (permission === "granted") {
+            // 在權限獲取後，即可開始定時推送通知
+            setInterval(displayNotification, 10000);
+          }
+        });
+      } else {
+        // 如果權限已經被授予，即可開始定時推送通知
+        setInterval(displayNotification, 10000);
+      }
     })
     .catch((error) => {
       console.error("服務註冊錯誤:", error);
@@ -26,18 +37,6 @@ function displayNotification() {
   }
 }
 
-// 請求推送權限
-if (Notification.permission !== "granted") {
-  Notification.requestPermission().then((permission) => {
-    if (permission === "granted") {
-      // 在權限獲取後，即可開始定時推送通知
-      setInterval(displayNotification, 10000);
-    }
-  });
-} else {
-  // 如果權限已經被授予，即可開始定時推送通知
-  setInterval(displayNotification, 10000);
-}
 //----鼠標圖示---------------------------------------------------------------//
 const cursor = document.querySelector(".cursor");
 const a = document.querySelectorAll(".a");
